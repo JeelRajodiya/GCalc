459cbfdb374b8879c25c6da949337691a1f38cac
jeel2331 <jeelrajodiyajeel@gmail.com>
2021-01-17 15:26:50 +0530

    build 27

---
+++ build/static/js/53.dd699d1c.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonptest=this.webpackJsonptest||[]).push([[53],{128:function(t,n,e){"use strict";e.r(n),e.d(n,"startStatusTap",(function(){return i}));var o=e(10),i=function(){var t=window;t.addEventListener("statusTap",(function(){Object(o.h)((function(){var n=t.innerWidth,e=t.innerHeight,i=document.elementFromPoint(n/2,e/2);if(i){var c=i.closest("ion-content");c&&c.componentOnReady().then((function(){Object(o.f)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.dd699d1c.chunk.js.map
